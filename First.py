c12b502f8b0a8e1adcedff108f14474fc889ef95
taskmaster56 <mishraganeshmishra123@gmail.com>
2024-06-05 20:51:31 +0530

    learning python

---
+++ First.py
@@ -0,0 +1,79 @@
+print("Hello world!")
+a=6
+b=8
+c=a+b
+print("addition of 2 numbers:",c) 
+
+
+#Arithmatic operator 
+a=2
+b=1
+print(a+b)
+print('gan'+'esh')
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b) #floor divison
+
+a=2
+b=3
+print(a**b) # power output will 8 , 2^3
+
+#Assignment operators 
+
+
+#comparison operator 
+     
+
+    
+# logical operator
+
+a=True
+b=False
+print(a and b)
+print(a or b)
+print( not b)
+
+#bitwise opertator
+
+e=2  #0010
+d=6  #0110
+print(e ^ d)
+print(e & d)
+print(e | d)
+
+print(d>>e)
+print(d<<e)
+
+
+x= 10 
+print( x is 5)
+print( x is 10)
+
+y=10 
+print( y is not 5)
+print( y is not 10)       
+
+
+a = 7 
+b = 9
+c = 1
+
+if a>b:
+    if a>c:
+        print("A is greaater")
+elif b>a:
+    if b>c:
+        print("b is greter")
+else :
+    print("c is greater then a and b")
+
+
+if a<c:
+    if a<b:
+        print("A is smaller")
+elif b<c:
+    if b<a:
+        print("b is smaller")
+else :
+    print("c is smaller6 then a and b")
---
+++ Pawan.py
@@ -0,0 +1,18 @@
+print("pawan is goodva boy")
+
+a=2
+b=1
+print(a+b)
+print('gan'+'esh')
+print(a-b)
+print(a*b)
+print(a/b)
+
+e=2  #0010
+d=6  #0110
+print(e ^ d)
+print(e & d)
+print(e | d)
+
+
+
